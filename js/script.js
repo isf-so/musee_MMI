0bbcd395c7c376d8c59ee212c096a5677b9f6c84
s <glcsofiane40@gmail.com>
2024-05-10 22:34:53 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-addEventListener
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".blurry-background").addEventListener("click", function() {
+        this.style.filter = "blur(20px)";
+        this.style.transition = "filter 0.5s ease";
+    });
+});
