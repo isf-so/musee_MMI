d661d2d42e9a17b32f563f37547e5dce5a8fb013
s <glcsofiane40@gmail.com>
2024-05-10 23:02:45 +0200

    test

---
+++ js/musee.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var blurEffect = document.querySelector(".blur-effect");
+    var container = document.querySelector(".container");
+
+    // Disparition du flou après 2 secondes
+    setTimeout(function() {
+        blurEffect.style.opacity = "0";
+        container.style.display = "block"; // Affichage du contenu après disparition du flou
+    }, 1000);
+});
--- js/script.js
+++ js/script.js
@@ -1,27 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
     var blurryBackground = document.querySelector(".blurry-background");
+    var fullScreenDiv = document.querySelector(".full-screen-div");
 
     blurryBackground.addEventListener("click", function() {
         this.style.filter = "blur(20px)";
         this.style.transition = "filter 0.5s ease";
 
-        // Suppression complète de la div blurry-background après 2 secondes
-        setTimeout(function() {
-            blurryBackground.style.display = "none";
-        }, 2000);
-
-        // Création d'une nouvelle div plein écran
-        var fullScreenDiv = document.createElement("div");
-        fullScreenDiv.classList.add("full-screen-div");
-        fullScreenDiv.style.position = "fixed";
-        fullScreenDiv.style.top = "0";
-        fullScreenDiv.style.left = "0";
-        fullScreenDiv.style.width = "100%";
-        fullScreenDiv.style.height = "100%";
-        fullScreenDiv.style.backgroundColor = "rgba(0, 0, 0, 0.5)"; // Couleur de fond semi-transparente
-        fullScreenDiv.style.zIndex = "9999"; // Au-dessus de tout
-
-        // Ajout de la nouvelle div au body
-        document.body.appendChild(fullScreenDiv);
+        // Ajout d'un événement pour la fin de l'animation de flou
+        this.addEventListener("transitionend", function() {
+            // Suppression complète de la div blurry-background après 2 secondes
+            setTimeout(function() {
+                blurryBackground.style.display = "none";
+                // Rediriger vers une autre page HTML après la suppression
+                window.location.href = "../pages/musee.html";
+            }, 2000);
+        });
     });
 });
