239d05c38fdaadf5c08756cc758b36cbe6130bd6
s <glcsofiane40@gmail.com>
2024-05-10 22:28:04 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,17 +1,1 @@
-const conteneur = document.getElementById('conteneur');
-
-conteneur.addEventListener('wheel', (event) => {
-    event.preventDefault(); // Empêche le défilement vertical
-
-    const delta = event.deltaY || event.detail || event.wheelDelta;
-
-    conteneur.scrollLeft += delta;
-});
-
-
-document.getElementById('centeredText').addEventListener('click', function() {
-    var element = document.querySelector('#centeredText');
-    element.parentNode.removeChild(element);
-});
-
-  
+addEventListener
