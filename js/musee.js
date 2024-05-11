bfee0eca32443234938867dec8b6022169f59ff1
s <glcsofiane40@gmail.com>
2024-05-11 14:12:26 +0200

    a

--- js/musee.js
+++ js/musee.js
@@ -1,20 +1,42 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function () {
+    var container = document.querySelector('.container');
+
+    container.addEventListener('wheel', function (event) {
+        event.preventDefault(); // Empêche le défilement par défaut
+
+        var delta = event.deltaY || event.detail || event.wheelDelta;
+
+        // Ajuste la position horizontale du défilement
+        container.scrollLeft += (delta < 0 ? -30 : 30);
+    });
+});
+  
+  
+document.addEventListener("DOMContentLoaded", function () {
     var blurEffect = document.querySelector(".blur-effect");
     var ecran = document.querySelector("#ecran");
     var container = document.querySelector(".container");
     var svg = document.querySelector("#svg");
-    var oeuvre = document.querySelector(".oeuvre");
+    var body = document.querySelector("body");
 
 
     // Transition de l'opacité du flou à 0
     blurEffect.style.transition = "opacity 1s ease-in-out";
     blurEffect.style.opacity = "0";
+    body.style.overflow = "hidden";
 
     // Disparition du flou après 2 secondes
-    setTimeout(function() {
+    setTimeout(function () {
         blurEffect.style.display = "none";
         blurEffect.style.opacity = ""; // Réinitialisation de l'opacité
         ecran.style.display = "none";
         svg.style.display = "block";
+        document.body.style.overflowX = "visible";
     }, 2000);
 });
+
+
+document.querySelector('#svg').addEventListener('click', transition);
+function transition() {
+    document.querySelector(".contenu3").classList.toggle('transition');
+}
