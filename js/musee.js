4bba3eb3258a4d375bdbbef4e33b080eb32401eb
s <glcsofiane40@gmail.com>
2024-05-11 13:20:13 +0200

    t

--- js/musee.js
+++ js/musee.js
@@ -1,10 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     var blurEffect = document.querySelector(".blur-effect");
+    var ecran = document.querySelector("#ecran");
     var container = document.querySelector(".container");
+    var svg = document.querySelector("#svg");
+    var oeuvre = document.querySelector(".oeuvre");
+
+
+    // Transition de l'opacité du flou à 0
+    blurEffect.style.transition = "opacity 1s ease-in-out";
+    blurEffect.style.opacity = "0";
 
     // Disparition du flou après 2 secondes
     setTimeout(function() {
         blurEffect.style.display = "none";
-        container.style.display = "block"; // Affichage du contenu après disparition du flou
-    }, 1000);
+        blurEffect.style.opacity = ""; // Réinitialisation de l'opacité
+        ecran.style.display = "none";
+        svg.style.display = "block";
+    }, 2000);
 });
